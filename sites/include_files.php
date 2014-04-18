093a9b964679fb56e44a0186aeef0d5404cfa5c4
nadobnyk <konrad@konrad-laptop.(none)>
2014-04-18 19:56:13 +0200

    debug

--- sites/include_files.php
+++ sites/include_files.php
@@ -8,19 +8,19 @@ $glob = glob("sites/*/php/*.php");
 require $glob[0];
 
 
-foreach (glob($plugins_dir+"*/*.php") as $php_plugin)
+foreach (glob($plugins_dir."*/*.php") as $php_plugin)
 {
     require_once $php_plugin;
 
 }
-foreach (glob($plugins_dir+"*/*.js") as $js_plugin)
+foreach (glob($plugins_dir."*/*.js") as $js_plugin)
 {
-    echo '<script type="text/javascript" src="'+$js_plugin+'"></script>';
+    echo '<script type="text/javascript" src="'.$js_plugin.'"></script>';
 }
 
-foreach (glob($plugins_dir+"*/*.css") as $css_plugin)
+foreach (glob($plugins_dir."*/*.css") as $css_plugin)
 {
-    echo '<script type="text/javascript" src="'+$css_plugin+'"></script>';
+    echo '<script type="text/javascript" src="'.$css_plugin.'"></script>';
 }
 
 foreach (glob("sites/*/php/*.php") as $php_widget)
@@ -28,14 +28,14 @@ foreach (glob("sites/*/php/*.php") as $php_widget)
     require_once $php_widget;
 }
 
-foreach (glob($widgets_dir+"*/js/*.js") as $js_widget)
+foreach (glob($widgets_dir."*/js/*.js") as $js_widget)
 {
-    echo '<script type="text/javascript" src="'+$js_widget+'"></script>';
+    echo '<script type="text/javascript" src="'.$js_widget.'"></script>';
 }
 
-foreach (glob($widgets_dir+"*/css/*.css") as $css_widget)
+foreach (glob($widgets_dir."*/css/*.css") as $css_widget)
 {
-    echo '<link rel="stylesheet" type="text/css" href="'+$css_widget+'">';
+    echo '<link rel="stylesheet" type="text/css" href="'.$css_widget.'">';
 }
 
 echo "<br>import->end<br>";
