6841309630c4fb2d111dec471c8997059b87caae
konrads <konrad.nadobny@gmail.com>
2014-04-18 20:45:28 +0200

    changes in weather widget

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	$(".weather_data").each(function(){
-		$(this).html(weather_data_now.find($(this.attr("id"))));
+
+$(".weather_data").each(function(){
+		$(this).html(weather_data_now.main.$(this).id);
 		//$(this).html(weather_data_now.main.find($(this.attr("id"))));
 	    //$("#temperature").html(weather_data_now.main.temp);
 	});
