d5b3999f1ef08232e2e2ccf5a9282eca45b7c95b
konrads <konrad.nadobny@gmail.com>
2014-04-18 20:17:24 +0200

    debug

---
+++ plugins/bootstrap/bootstrap.php
@@ -0,0 +1,10 @@
+<link href="<?php $plugins_dir ?>css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php $plugins_dir ?>js/bootstrap.min.js"></script>
