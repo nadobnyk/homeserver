7920c79024bec131564935df948892c6c48933da
konrads <konrad.nadobny@gmail.com>
2014-04-18 22:32:45 +0200

    debug

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ require_once 'include_files.php';
 <div id="weather_container">
 	<div id="weather_today">
 		<ul>
-			<li>Wetter:<span class="weather_data" id="weather.description"></span></li>
-			<li>Temperatur:<span class="weather_data" id="main.temp"></span></li>
-			<li>Regenwarscheinlichkeit:<span class="weather_data" id="rain.3h"></span></li>
-			<li>Luftfeuchtigkeit:<span class="weather_data" id="main.humidity"></span></li>
-			<li>Wind:<span class="weather_data" id="wind.speed"></span></li>
+			<li>Wetter:<span class="weather_data" id="description"></span>°C</li>
+			<li>Temperatur:<span class="weather_data" id="temp"></span> (<span class="weather_data" id="temp_min"></span> - <span class="weather_data" id="temp_max"></span>)</li>
+			<li>Regenwarscheinlichkeit:<span class="weather_data" id="rain"></span></li>
+			<li>Luftfeuchtigkeit:<span class="weather_data" id="humidity"></span></li>
+			<li>Wind:<span class="weather_data" id="wind_speed"></span>km/h <span class="weather_data" id="wind_deg"></span>°</li>
 	</div>
 </div>
--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-	
+$(document).ready(function(){	
 	$('.weather_data[id="description"]').html(weather_data_now.weather[0].description);
 	$('.weather_data[id="temp"]').html(weather_data_now.main.temp);
 	$('.weather_data[id="temp_min"]').html(weather_data_now.main.temp_min);
