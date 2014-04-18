a99e4e389888739715027a8272afe21bf237df9c
konrads <konrad.nadobny@gmail.com>
2014-04-18 22:14:02 +0200

    debug

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 	$(".weather_data").each(function(){
-		$(this).html(weather_data_now[$(this).attr("id")]);
-
+		find_weather_data($(this).attr("id"), weather_data_now)
 	});
+
+	function find_weather_data(key, data){
+		if(data[key].children() > 0){
+			throw new Error((String)data+"Has Child")
+			data[key].children().each(function(){
+				find_weather_data(key, $(this)));
+			});
+	    }else{
+	    	if(data.hasOwnProperty(key)){
+	    		$('.weather_data[id="'+key+'"]')html(data[key]);
+	    	}
+	    }
+	}
 });
