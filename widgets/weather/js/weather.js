a1edd1882b1df62e514634ae7f7dc6af467a5fa3
konrads <konrad.nadobny@gmail.com>
2014-04-18 22:14:47 +0200

    debug

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 
 	function find_weather_data(key, data){
 		if(data[key].children() > 0){
-			throw new Error((String)data+"Has Child")
 			data[key].children().each(function(){
 				find_weather_data(key, $(this)));
 			});
