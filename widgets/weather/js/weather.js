b11558220895eabfdc6357a74958c7a90e677b54
konrads <konrad.nadobny@gmail.com>
2014-04-18 20:54:02 +0200

    changes in weather widget

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
-	var my_weather_data_now_array = $.parseJSON(weather_data_now);
 	var myDomObject;
 	$(".weather_data").each(function(){
 		myDomObject = $(this);
-		myID = $(this),attr("id");
+		myID = $(this).attr("id");
 		
-		$.each(my_weather_data_now_array, function(){
+		$.each(weather_data_now, function(){
 			if($(this)[myID]){
 				myDomObject.html($(this)[myID]);
 			}
