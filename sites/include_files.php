6fb8557cecdb6e2da926f84be990c52ef984d4d1
nadobnyk <konrad@konrad-laptop.(none)>
2014-04-18 19:00:13 +0200

    adjusted paths in iclude script

--- sites/include_files.php
+++ sites/include_files.php
@@ -1,32 +1,35 @@
 <?php
 echo "<br>import->start<br>";
-foreach (glob("/sites/*/php/*.php") as $php_file)
-{
-    include $php_file;
-}
 
-foreach (glob("/plugins/*/*.php") as $php_plugin)
+$plugins_dir ="../plugins/";
+$widgets_dir = "";
+
+foreach (glob($plugins_dir+"*/*.php") as $php_plugin)
 {
     include $php_plugin;
 }
-foreach (glob("/plugins/*/*.js") as $js_plugin)
+foreach (glob($plugins_dir+"*/*.js") as $js_plugin)
 {
     echo '<script type="text/javascript" src="'+$js_plugin+'"></script>';
 }
 
-foreach (glob("/plugins/*/*.css") as $css_plugin)
+foreach (glob($plugins_dir+"*/*.css") as $css_plugin)
 {
     echo '<script type="text/javascript" src="'+$css_plugin+'"></script>';
 }
 
-foreach (glob("/sites/*/js/*.js") as $js_file)
+foreach (glob($widgets_dir+"*/php/*.php") as $php_widget)
 {
-    echo '<script type="text/javascript" src="'+$js_file+'"></script>';
+    include $php_widget;
+}
+foreach (glob($widgets_dir+"*/js/*.js") as $js_widget)
+{
+    echo '<script type="text/javascript" src="'+$js_widget+'"></script>';
 }
 
-foreach (glob("/sites/*/css/*.css") as $css_file)
+foreach (glob($widgets_dir+"*/css/*.css") as $css_widget)
 {
-    echo '<link rel="stylesheet" type="text/css" href="'+$css_file+'">';
+    echo '<link rel="stylesheet" type="text/css" href="'+$css_widget+'">';
 }
 
 echo "<br>import->end<br>";
