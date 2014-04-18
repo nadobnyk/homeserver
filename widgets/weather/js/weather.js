59bb44eeb43ceca88d23bbb52cfd673d827031ed
konrads <konrad.nadobny@gmail.com>
2014-04-18 21:05:38 +0200

    debug

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
 	alert(weather_data_now);
-
-	for(key in weather_data_now){                               
-	    if(key.indexOf("main") != -1){           
-	        $("#description").html(weather_data_now[key]);     
-	        break;
-	    }
-	}
-
-
+    $("#description").html(weather_data_now.getElementById("main"));     
 });
