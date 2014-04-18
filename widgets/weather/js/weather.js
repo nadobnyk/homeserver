57cbad83b2fa91a16293147d21f88ad145d1787c
konrads <konrad.nadobny@gmail.com>
2014-04-18 22:29:24 +0200

    debug

--- widgets/weather/js/weather.js
+++ widgets/weather/js/weather.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	
-	$(".weather_data[id=description]").html(weather_data_now.weather[0].description);
-	$(".weather_data[id=temp").html(weather_data_now.main.temp);
-	$(".weather_data[id=temp_min").html(weather_data_now.main.temp_min);
-	$(".weather_data[id=temp_max").html(weather_data_now.main.temp_max);
-	$(".weather_data[id=icon").html(weather_data_now.weather[0].icon);
-	$(".weather_data[id=humidity").html(weather_data_now.main.humidity);
-	$(".weather_data[id=wind_speed").html(weather_data_now.wind.speed);
-	$(".weather_data[id=wind_deg").html(weather_data_now.wind.deg);
-	$(".weather_data[id=rain").html(weather_data_now.rain.3h);
+	$('.weather_data[id="description"]').html(weather_data_now.weather[0].description);
+	$('.weather_data[id="temp"]').html(weather_data_now.main.temp);
+	$('.weather_data[id="temp_min"]').html(weather_data_now.main.temp_min);
+	$('.weather_data[id="temp_max"]').html(weather_data_now.main.temp_max);
+	$('.weather_data[id="icon"]').html(weather_data_now.weather[0].icon);
+	$('.weather_data[id="humidity"]').html(weather_data_now.main.humidity);
+	$('.weather_data[id="wind_speed"]').html(weather_data_now.wind.speed);
+	$('.weather_data[id="wind_deg"]').html(weather_data_now.wind.deg);
+	$('.weather_data[id="rain"]').html(weather_data_now.rain.3h);
 });
