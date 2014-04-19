9e2f6fd4529ad7084baea5671e28d03c8e6d8826
konrads <konrad.nadobny@gmail.com>
2014-04-19 14:30:13 +0200

    placed clock in widgets dir

---
+++ widgets/clock/clock.php
@@ -0,0 +1,71 @@
+<div id="clock">
+	<div id="a">
+		<div id="b">
+			<div id="c">
+				<div id="d">
+					<div id="sh">
+						<div class="hh">
+							<div class="h"></div>
+						</div>
+						<div class="mm">
+							<div class="m"></div>
+							<div class="mr"></div>
+						</div>
+						<div class="ss">
+							<div class="s"></div>
+						</div>
+					</div>
+					<div id="ii">
+						<div class="pure">
+						<b><i><i><i><i></i></i></i></i>
+							<b><i><i><i><i></i></i></i></i>
+								<b><i><i><i><i></i></i></i></i>
+									<b><i><i><i><i></i></i></i></i>
+										<b><i><i><i><i></i></i></i></i>
+											<b><i><i><i><i></i></i></i></i></b>
+										</b>
+									</b>
+								</b>
+							</b>
+						</b></div>
+						<!-- this is need only to show 
+							bugs-free variant in many browsers -->
+						<div class="fixed">
+						<b><i></i><i></i><i></i><i></i></b>
+						<b><i></i><i></i><i></i><i></i></b>
+						<b><i></i><i></i><i></i><i></i></b>
+						<b><i></i><i></i><i></i><i></i></b>
+						<b><i></i><i></i><i></i><i></i></b>
+						<b><i></i><i></i><i></i><i></i></b>
+						</div>
+						<!-- till here -->
+					</div>
+					<div id="e">
+						<div id="f">
+							<u>12<u>1<u>2<u>3</u>4</u>5</u></u>
+						</div>
+						<div id="g">
+							<u><u>11<u>10<u>9</u>8</u>7</u>6</u>
+						</div>
+						<div id="q">quartz</div>
+					</div>
+					<div class="hh">
+						<div class="h"></div>
+					</div>
+					<div class="mm">
+						<div class="m"></div>
+						<div class="mr"></div>
+					</div>
+					<div class="ss">
+						<div class="s"></div>
+						<div class="sr"></div>
+					</div>
+					<div id="k"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="css3icon"/>
+</body>
+</html>
